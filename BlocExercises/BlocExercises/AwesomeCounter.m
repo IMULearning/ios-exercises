b40a2b81af96e1d7bf7ab35646bb0c338c749048
Weinan Qiu <davidiamyou@gmail.com>
2015-08-18 17:15:15 -0400

    Checkpoint 12 Assignment

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -11,8 +11,22 @@
 @implementation AwesomeCounter
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @"";
+    NSString *result = @"";
+    NSInteger low = number;
+    NSInteger high = otherNumber;
+    
+    if (low > high) {
+        NSInteger temp;
+        temp = low;
+        low = high;
+        high = temp;
+    }
+    
+    for (NSInteger i = low; i <= high; i++) {
+        result = [result stringByAppendingString:[NSString stringWithFormat:@"%ld", i]];
+    }
+    
+    return result;
 }
 
 @end
