f96d0f140e8429f5d94f1f36c687c281a49f4c24
Weinan Qiu <davidiamyou@gmail.com>
2015-08-18 22:48:22 -0400

    Checkpoint 18 Assignment

--- SwiftExercises.playground/section-1.swift
+++ SwiftExercises.playground/section-1.swift
@@ -8,7 +8,7 @@ Strings
 
 func favoriteCheeseStringWithCheese(cheese: String) -> String {
     // WORK HERE
-    return cheese
+    return "My favorite cheese is  \(cheese)"
 }
 
 let fullSentence = favoriteCheeseStringWithCheese("cheddar")
@@ -23,10 +23,15 @@ Arrays & Dictionaries
 let numberArray = [1, 2, 3, 4]
 // Add 5 to this array
 // WORK HERE
+var mutableNumberArray = numberArray
+mutableNumberArray.append(5)
 
 let numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
 // Add 5 : "five" to this dictionary
 // WORK HERE
+var mutableNumberDictionary = numberDictionary
+mutableNumberDictionary[5] = "five"
+mutableNumberDictionary
 
 /*
 
@@ -36,9 +41,15 @@ Loops
 
 // Use a closed range loop to print 1 - 10, inclusively
 // WORK HERE
+for i in (1...10) {
+    print(i)
+}
 
 // Use a half-closed range loop to print 1 - 10, inclusively
 // WORK HERE
+for i in (1..<11) {
+    print(i)
+}
 
 let worf = [
     "name": "Worf",
@@ -56,9 +67,11 @@ let picard = [
 let characters = [worf, picard]
 
 func favoriteDrinksArrayForCharacters(characters:Array<Dictionary<String, String>>) -> Array<String> {
-    // return an array of favorite drinks, like ["prune juice", "tea, Earl Grey, hot"]
-    // WORK HERE
-    return []
+    var results: Array<String> = []
+    for item in characters {
+        results.append(item["favorite drink"] ?? "")
+    }
+    return results
 }
 
 let favoriteDrinks = favoriteDrinksArrayForCharacters(characters)
@@ -76,8 +89,10 @@ Functions
 let strings = ["milk", "eggs", "bread", "challah"]
 
 // WORK HERE - make your function and pass `strings` in
-
-let expectedOutput = "milk;eggs;bread;challah"
+func semicolonSeperatedStringFromArray(input: Array<String>) -> String {
+    return input.join(";")
+}
+let expectedOutput = semicolonSeperatedStringFromArray(strings)
 
 /*
 
@@ -89,3 +104,5 @@ let cerealArray = ["Golden Grahams", "Cheerios", "Trix", "Cap'n Crunch OOPS! All
 
 // Use a closure to sort this array alphabetically
 // WORK HERE
+var mutableArray = cerealArray
+sorted(mutableArray, { a, b in a > b})
